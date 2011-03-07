9569a8f204807858c37721abde88bbb4ccf1a604
rwldrn <waldron.rick@gmail.com>
2011-03-07 17:12:23 -0500

    pre-imple

---
+++ ready/layout.application.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function () { 
+
+  var outerLayout, middleLayout, innerLayout; 
+
+  outerLayout = $('body').layout({ 
+    center__paneSelector: ".outer-center" 
+  , west__paneSelector:   ".outer-west" 
+  , east__paneSelector:   ".outer-east" 
+  , west__size:       350 
+ 
+  , spacing_open:     8 // ALL panes
+  
+  , north__spacing_open:  0
+  , south__spacing_open:  0
+  , west__onresize:   "innerLayout.resizeAll" 
+  , resizable: false
+  
+  }); 
+
+  middleLayout = $('div.outer-center').layout({ 
+    center__paneSelector: ".middle-center" 
+  
+  , north__size:       400
+  , spacing_open:     8  // ALL panes
+  , spacing_closed:     12 // ALL panes
+  , center__onresize:   "innerLayout.resizeAll" 
+  }); 
+
+
+}); 
