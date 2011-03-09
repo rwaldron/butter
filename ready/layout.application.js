067ecd291bfb83fef8f3c143e208aeb981bef93f
rwldrn <waldron.rick@gmail.com>
2011-03-09 10:51:28 -0500

    Re-layout stable

--- ready/layout.application.js
+++ ready/layout.application.js
@@ -14,18 +14,22 @@ $(document).ready(function () {
   
   , north__spacing_open:  0
   , south__spacing_open:  0
-  , west__onresize:   "outerLayout.resizeAll" 
+  //, west__onresize:   "outerLayout.resizeAll" 
   , resizable: false
+  , closable: false
   
   }); 
 
+  
   middleLayout = $('div.outer-center').layout({ 
     center__paneSelector: ".middle-center" 
   
-  , north__size:       400
-  , spacing_open:     8  // ALL panes
-  , spacing_closed:     12 // ALL panes
-  , center__onresize:   "innerLayout.resizeAll" 
+  , north__size:       395
+  //, spacing_open:     8  // ALL panes
+  //, spacing_closed:     12 // ALL panes
+  //, center__onresize:   "innerLayout.resizeAll" 
+  , resizable: false
+  , closable: false
   }); 
 
 
